35753866d30ef61b6e39672b6a337ddea97c9e15
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-01-31 19:10:48 +0700

    update code in fetch news function

--- jd-post/src/fetch-news.ts
+++ jd-post/src/fetch-news.ts
@@ -1,8 +1,10 @@
 import { News } from "@/types"
 
-export const fetchNews = async (page: string) => {
+export const fetchNews = async (slug: string) => {
     const apikey = process.env.API_KEY
-    const data = await fetch(`https://api.nytimes.com/svc/topstories/v2/${page}.json?api-key=${apikey}`)
+    const data = await fetch(`https://api.nytimes.com/svc/topstories/v2/${slug}.json?api-key=${apikey}`, {
+        cache: 'no-store'
+    })
     const news = await data.json()
     return news.results as News[]
 }
