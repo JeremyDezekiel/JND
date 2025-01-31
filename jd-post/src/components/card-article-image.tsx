c5134412794d190e00a40d0a8a0066fa68c5b485
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-01-31 19:11:40 +0700

    make component card article image to make it reusable

---
+++ jd-post/src/components/card-article-image.tsx
@@ -0,0 +1,23 @@
+import { formatRelativeTime, formatSection } from "@/formats"
+import { News } from "@/types"
+
+export default function CardArticleImage({ news }: { news: News }) {
+    return (
+        <a href={news.url} target="_blank" rel="noopener noreferrer">
+            <div className="flex flex-col gap-5 border-b-2 group cursor-pointer h-fit">
+                <img className="w-fit self-center" src={news.multimedia[1].url} alt={news.multimedia[1].caption} />
+                <h3 className="font-bold text-xl group-hover:underline">{news.title}</h3>
+                <p>{news.abstract}</p>
+                <div className="pb-2">
+                    <span className="pe-5">{formatRelativeTime(news.updated_date)}</span>
+                    {
+                        news.section !== '' && <span className="px-5 border-s-2">{formatSection(news.section)}</span>
+                    }
+                    {
+                        news.subsection !== "" && <span className="ps-5 border-s-2">{formatSection(news.subsection)}</span>
+                    }
+                </div>
+            </div>
+        </a>
+    )
+}
