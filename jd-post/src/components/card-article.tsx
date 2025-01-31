a338c44aef9c2a95bf79c984824bdb9622d51393
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-01-31 19:12:05 +0700

    make component card article to make it reusable

---
+++ jd-post/src/components/card-article.tsx
@@ -0,0 +1,19 @@
+import { formatRelativeTime, formatSection } from "@/formats";
+import { News } from "@/types";
+
+export default function CardArticle({ news }: { news: News }) {
+    return (
+        <a href={news.url} target="_blank" rel="noopener noreferrer">
+            <div className="flex flex-col gap-5 border-b-2 group cursor-pointer">
+                <h1 className="font-bold text-xl group-hover:underline">{news.title}</h1>
+                <p>{news.abstract}</p>
+                <div className="pb-2">
+                    <span className="pe-5">{formatRelativeTime(news.updated_date)}</span>
+                    {
+                        news.section !== "" && <span className="ps-5 border-s-2">{formatSection(news.section)}</span>
+                    }
+                </div>
+            </div>
+        </a>
+    )
+}
