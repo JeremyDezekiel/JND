0190c10656a17d242abe91ee6215772bb43accdf
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-01-31 19:13:51 +0700

    update design in component top 1 article

--- jd-post/src/components/top-1-article.tsx
+++ jd-post/src/components/top-1-article.tsx
@@ -3,27 +3,33 @@ import { News } from "@/types"
 export default function Top1Article({ news, page }: { news: News, page: string }) {
     return (
         page === "home" ? (
-            <div className="flex flex-col gap-5 group cursor-pointer">
-                <img className="w-full" src={news.multimedia[0].url} alt={news.multimedia[0].caption} />
-                <h1 className="font-bold text-4xl group-hover:underline">{news.title}</h1>
-                <p>{news.abstract}</p>
-            </div>
+            <a href={news.url} target="_blank" rel="noopener noreferrer">
+                <div className="flex flex-col gap-5 group cursor-pointer">
+                    <img className="w-full" src={news.multimedia[0].url} alt={news.multimedia[0].caption} />
+                    <h1 className="font-bold text-4xl group-hover:underline">{news.title}</h1>
+                    <p>{news.abstract}</p>
+                </div>
+            </a>
         ) : page === "sports" ? (
-            <div className="col-span-3 flex gap-5 group cursor-pointer">
-                <div className="flex flex-col gap-5">
-                    <h1 className="text-3xl group-hover:underline">{news.title}</h1>
-                    <p>{news.abstract}</p>
+            <a href={news.url} target="_blank" rel="noopener noreferrer">
+                <div className="flex gap-5 group cursor-pointer h-full">
+                    <div className="flex flex-col gap-5">
+                        <h1 className="text-3xl group-hover:underline">{news.title}</h1>
+                        <p>{news.abstract}</p>
+                    </div>
+                    <img src={news.multimedia[1].url} alt={news.multimedia[1].caption} />
                 </div>
-                <img src={news.multimedia[1].url} alt={news.multimedia[1].caption} />
-            </div>
+            </a>
         ) : (
-            <div className="relative w-full h-[50vh] group cursor-pointer">
-                <img className="w-full h-full" src={news.multimedia[0].url} alt={news.multimedia[0].caption} />
-                <div className="absolute bottom-0 left-0 p-5 bg-black text-white flex flex-col gap-3 w-2/5">
-                    <h1 className="font-bold text-3xl group-hover:underline">{news.title}</h1>
-                    <p>{news.abstract}</p>
+            <a href={news.url} target="_blank" rel="noopener noreferrer">
+                <div className="relative w-full h-[50vh] group cursor-pointer">
+                    <img className="w-full h-full" src={news.multimedia[0].url} alt={news.multimedia[0].caption} />
+                    <div className="absolute bottom-0 left-0 p-5 bg-black text-white flex flex-col gap-3 w-2/5">
+                        <h1 className="font-bold text-3xl group-hover:underline">{news.title}</h1>
+                        <p>{news.abstract}</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         )
     )
 }
