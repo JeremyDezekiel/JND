195c9308d49d3d4c629050e9c32ce392dce06370
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-02-03 03:08:47 +0700

    add function load more on more news page

---
+++ jd-post/src/components/more-news-pagination.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import CardMoreNews from "@/components/cards/card-more-news"
+import { useState } from "react";
+
+export default function MoreNewsPagination({ news }: { news: News[] }) {
+
+    const [maxNews, setMaxNews] = useState(10)
+
+    return (
+        <div className="flex flex-col gap-10 justify-center mx-40 mt-10">
+            {
+                news.slice(0, 0 + maxNews).map((item, idx) => (
+                    <CardMoreNews key={idx} news={item} />
+                ))
+            }
+            {
+                maxNews < news.length && (
+                    <button
+                        className="border py-2 px-5 rounded-lg w-fit self-end hover:bg-white hover:text-black"
+                        onClick={() => setMaxNews(prevValue => prevValue + 10)}
+                        disabled={maxNews >= news.length}
+                    >
+                        Load More
+                    </button>
+                )
+            }
+        </div>
+    )
+}
