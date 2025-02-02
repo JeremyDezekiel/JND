29113a724df9868b2eaf27c973999bff57d6b5f7
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-02-03 03:06:51 +0700

    add function and fix design on footer

--- jd-post/src/components/footer.tsx
+++ jd-post/src/components/footer.tsx
@@ -1,41 +1,69 @@
+"use client"
+
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+    const pathname = usePathname()
+
     return (
         <div>
             <div className="container mx-auto">
-                <Image
-                    className="my-5 w-32 h-auto cursor-pointer"
-                    src="/assets/dark-mode.png"
-                    alt="Logo"
-                    width={150}
-                    height={50}
-                    priority
-                />
+                <div className="hidden dark:flex">
+                    <Link href="/">
+                        <Image
+                            className="my-5 w-32 h-auto cursor-pointer"
+                            src="/assets/dark-mode.png"
+                            alt="logo-dark"
+                            width={150}
+                            height={50}
+                            priority
+                        />
+                    </Link>
+                </div>
+                <div className="flex dark:hidden">
+                    <Link href="/">
+                        <Image
+                            className="my-5 w-32 h-auto cursor-pointer"
+                            src="/assets/light-mode.png"
+                            alt="logo-light"
+                            width={150}
+                            height={50}
+                            priority
+                        />
+                    </Link>
+                </div>
                 <div className="flex gap-5 mb-5 border-b-2 pb-5">
-                    <h5 className="cursor-pointer hover:underline">Home</h5>
-                    <h5 className="cursor-pointer hover:underline">News</h5>
-                    <h5 className="cursor-pointer hover:underline">Sport</h5>
-                    <h5 className="cursor-pointer hover:underline">Business</h5>
-                    <h5 className="cursor-pointer hover:underline">Innovation</h5>
-                    <h5 className="cursor-pointer hover:underline">Culture</h5>
-                    <h5 className="cursor-pointer hover:underline">Arts</h5>
-                    <h5 className="cursor-pointer hover:underline">Travel</h5>
-                    <h5 className="cursor-pointer hover:underline">Earth</h5>
-                    <h5 className="cursor-pointer hover:underline">Video</h5>
-                    <h5 className="cursor-pointer hover:underline">Live</h5>
-                    <h5 className="cursor-pointer hover:underline">Audio</h5>
-                    <h5 className="cursor-pointer hover:underline">Weather</h5>
-                    <h5 className="cursor-pointer hover:underline">JND Shop</h5>
+                    <Link href="/" className={`hover:underline ${pathname === "/" || pathname === "/more-news/home" ? "underline" : ""}`}>Home</Link>
+                    <Link href="/section/sports" className={`hover:underline ${pathname === "/section/sports" || pathname === "/more-news/sports" ? "underline" : ""}`}>Sports</Link>
+                    <Link href="/section/business" className={`hover:underline ${pathname === "/section/business" || pathname === "/more-news/business" ? "underline" : ""}`}>Business</Link>
+                    <Link href="/section/fashion" className={`hover:underline ${pathname === "/section/fashion" || pathname === "/more-news/fashion" ? "underline" : ""}`}>Fashion</Link>
+                    <Link href="/section/science" className={`hover:underline ${pathname === "/section/science" || pathname === "/more-news/science" ? "underline" : ""}`}>Science</Link>
+                    <Link href="/section/arts" className={`hover:underline ${pathname === "/section/arts" || pathname === "/more-news/arts" ? "underline" : ""}`}>Arts</Link>
+                    <Link href="/section/movies" className={`hover:underline ${pathname === "/section/movies" || pathname === "/more-news/movies" ? "underline" : ""}`}>Movies</Link>
+                    <Link href="/section/world" className={`hover:underline ${pathname === "/section/world" || pathname === "/more-news/world" ? "underline" : ""}`}>World</Link>
+                    <Link href="/section/food" className={`hover:underline ${pathname === "/section/food" || pathname === "/more-news/food" ? "underline" : ""}`}>Food</Link>
+                    <Link href="/section/travel" className={`hover:underline ${pathname === "/section/travel" || pathname === "/more-news/travel" ? "underline" : ""}`}>Travel</Link>
                 </div>
                 <div className="flex gap-10 items-center mb-5">
                     <h2>Follow JND on:</h2>
                     <div className="flex gap-5">
-                        <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=YfCbGWCWcuar&format=png&color=FFFFFF" alt="X" />
-                        <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=118468&format=png&color=FFFFFF" alt="facebook" />
-                        <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=32292&format=png&color=FFFFFF" alt="instagram" />
-                        <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=84521&format=png&color=FFFFFF" alt="tiktok" />
-                        <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=37325&format=png&color=FFFFFF" alt="youtube" />
+                        <a href="https://x.com/" target="_blank" rel="noopener noreferrer">
+                            <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=YfCbGWCWcuar&format=png&color=FFFFFF" alt="X" />
+                        </a>
+                        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                            <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=118468&format=png&color=FFFFFF" alt="facebook" />
+                        </a>
+                        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                            <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=32292&format=png&color=FFFFFF" alt="instagram" />
+                        </a>
+                        <a href="https://www.tiktok.com/" target="_blank" rel="noopener noreferrer">
+                            <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=84521&format=png&color=FFFFFF" alt="tiktok" />
+                        </a>
+                        <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
+                            <img className="size-5 hover:border cursor-pointer" src="https://img.icons8.com/?size=100&id=37325&format=png&color=FFFFFF" alt="youtube" />
+                        </a>
                     </div>
                 </div>
                 <ul className="flex gap-5 text-sm mb-5">
