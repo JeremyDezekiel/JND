5ab80ab5a3407e687a00de10c8b3086b715ced91
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-02-03 03:02:37 +0700

    add new function to search news

--- jd-post/src/fetch-news.ts
+++ jd-post/src/fetch-news.ts
@@ -1,10 +1,36 @@
-import { News } from "@/types"
-
-export const fetchNews = async (slug: string) => {
-    const apikey = process.env.API_KEY
+export const fetchNews = async (slug: string): Promise<News[]> => {
+    const apikey = process.env.NEXT_PUBLIC_API_KEY
     const data = await fetch(`https://api.nytimes.com/svc/topstories/v2/${slug}.json?api-key=${apikey}`, {
-        cache: 'no-store'
+        next: {
+            revalidate: 3600
+        }
     })
-    const news = await data.json()
-    return news.results as News[]
+
+    if (!data.ok) {
+        throw new Error("Failed to fetch data")
+    }
+
+    const server: Server = await data.json()
+    let news: News[] = server.results
+    news = news.filter(item => item.section !== "admin")
+    news= news.filter(item => item.multimedia !== null)
+
+    return news as News[]
+}
+
+export const searchFetchNews = async (search: string): Promise<ResultsSearch[]> => {
+    const apikey = process.env.NEXT_PUBLIC_API_KEY
+    const data = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${apikey}`)
+    
+    if (!data.ok) {
+        throw new Error("Failed to fetch data")
+    }
+
+    const server: ServerSearch = await data.json()
+    const serverResponse: Response = server.response
+    let newsSearch: ResultsSearch[] = serverResponse.docs
+    newsSearch = newsSearch.filter(item => item.multimedia !== null)
+    newsSearch = newsSearch.filter(item => item.multimedia[0].url !== null)
+
+    return newsSearch as ResultsSearch[]
 }
